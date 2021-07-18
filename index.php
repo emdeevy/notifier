12f5ea3dbcdfffab4e6ddd36301cb730c0dded78
Denis Makula <emdeevy@gmail.com>
2021-07-18 10:49:39 +0000

    Submodules and placeholders for base files;

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo("test");
+    <head>
+
+        <title>Natural HR Notifier</title>
+
+        <meta charset="utf-8">
+
+        <script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
+        <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
+        <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+
+        <script type="text/babel" src="js/react/out/Card.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    </head>
+
+    <body>
+
+        <div id="login-register"></div>
+
+        <script type="text/babel">
+            
+            ReactDOM.render(
+                <Card />,
+                document.getElementById('login-register')
+            );
+
+        </script>
+    </body>
+
+</html>
